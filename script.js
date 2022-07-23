52ed72ef70db0d5c26a0cea4672138162991d4b1
Rohan James <rohanjames9282@gmail.com>
2022-07-23 21:59:11 +0530

    Display value of slider on HTML

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Displaying slider value in HTML
+const slider = document.querySelector(".slider");
+
+function getSliderVal(){
+     const gridSize = document.querySelector(".grid-size");
+     
+     gridSize.textContent = `${slider.value} x ${slider.value}`;
+        
+
+        
+    }
+
+
