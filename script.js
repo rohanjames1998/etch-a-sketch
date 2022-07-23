161fa2a3bd28cd79513ef9edb99ad13d33b7f1ae
Rohan James <rohanjames9282@gmail.com>
2022-07-23 22:13:53 +0530

    Add slider thumb size change according to slider value

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const slider = document.querySelector(".slider");
 
 function getSliderVal(){
      const gridSize = document.querySelector(".grid-size");
-     
      gridSize.textContent = `${slider.value} x ${slider.value}`;
-        
-
+    const thumbSize = `${slider.value}px`;
+     slider.style.setProperty("--thumbSize", thumbSize);
+   
         
     }
 
