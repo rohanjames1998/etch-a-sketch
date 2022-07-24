046eca8563e943aa20f2b01ff2f97197f9e01262
Rohan James <rohanjames9282@gmail.com>
2022-07-24 14:44:21 +0530

    Add function to increase and decrease divs inside canvas dynamically

--- script.js
+++ script.js
@@ -6,8 +6,49 @@ function getSliderVal(){
      gridSize.textContent = `${slider.value} x ${slider.value}`;
     const thumbSize = `${slider.value}px`;
      slider.style.setProperty("--thumbSize", thumbSize);
-   
-        
+
+     return setCanvasStyle();
     }
 
+    // Adding adjusts appropriate amount of rows and cols in canvas.
 
+    function setCanvasStyle(){
+        const sliderVal = slider.value;
+        const canvas = document.querySelector(".canvas");
+        
+        // Adding rows and cols.
+        canvas.style.setProperty( "--divs", `repeat(${sliderVal}, 1fr)`);
+        
+    addDivs();
+    }
+
+    // Adding appropriate amount of divs inside canvas.
+ 
+   function addDivs(){
+        const sliderVal = slider.value * slider.value;
+        const canvas = document.querySelector(".canvas");
+
+        // If divs are less than slidervalue then add divs
+        if (canvas.childElementCount < sliderVal){
+        for(let i = canvas.childElementCount; i < sliderVal; i++){
+        let emptyDiv = document.createElement("div");
+        emptyDiv.classList.add("empty-div");
+        canvas.appendChild(emptyDiv);
+        }  
+    }
+
+        // IF divs are more than slidervalue then remove divs
+        let divsToRemove = canvas.lastElementChild;
+        if(canvas.childElementCount > sliderVal){
+            for(let j = sliderVal; j < canvas.childElementCount; j++){
+                canvas.removeChild(divsToRemove);
+                divsToRemove = canvas.lastElementChild;
+            }
+        }
+   
+   }
+
+
+
+   
+   
