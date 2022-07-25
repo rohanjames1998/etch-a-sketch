aa2f09ebb1edfab41274c54cc2860b569db018c1
Rohan James <rohanjames9282@gmail.com>
2022-07-25 22:40:55 +0530

    Fixed rainbow color not removing when selecting other colors

--- script.js
+++ script.js
@@ -2,8 +2,12 @@
 // SLIDER/CANVAS FUNCTIONS
 /* -------------------- */
 
+
+
+
 // Displaying slider value in HTML
 const slider = document.querySelector(".slider");
+const canvas = document.querySelector(".canvas");
 
 function getSliderVal() {
   const gridSize = document.querySelector(".grid-size");
@@ -11,14 +15,13 @@ function getSliderVal() {
   const thumbSize = `${slider.value}px`;
   slider.style.setProperty("--thumbSize", thumbSize);
 
-  return setCanvasStyle(), addDivs(), canvasColorInput();
+  return setCanvasStyle(), addDivs()
 }
 
 // Adding adjusts appropriate amount of rows and cols in canvas.
 
 function setCanvasStyle() {
   const sliderVal = slider.value;
-  const canvas = document.querySelector(".canvas");
 
   // Adding rows and cols.
   canvas.style.setProperty("--divs", `repeat(${sliderVal}, 1fr)`);
@@ -47,49 +50,58 @@ function addDivs() {
   }
 }
 
+
+
 /* -------------------- */
 // COLOR FUNCTIONS
 /* -------------------- */
+
+
+
+
 //Tells canvasColorInput if the user is painting (i.e., rightclicked and hovering the canvas)
 
 let hex ="#000"; /* this value can be dynamically changed using buttons in color palette. */
 // This function applies the specified pen color (hex variable) to canvas when user interacts with it.
 let mouseDown = false;
+let isRainbowOn = false;
 document.addEventListener("mousedown", () => mouseDown = true);
 document.addEventListener("mouseup", () => mouseDown = false);
 
  // Making use user clicks and drags to paint.
-function canvasColorInput() {
-    // const emptyDiv = document.querySelectorAll(".empty-div");
-    // emptyDiv.forEach((item) =>
-    //   item.addEventListener(
-    //     "mouseover", () => { if (mouseDown === true){
-    //         item.style.backgroundColor = hex}
-    //     }
-    //   )
-    // );
 
-    document.addEventListener("mouseover", e => {
+
+  document.addEventListener("mouseover", function colorDiv(e){
       if(e.target.matches(".empty-div")){
         if (mouseDown == true){
           e.target.style.backgroundColor = hex;
         }
       }
-    } )
+    })
     
-  }
+  
 
 
 
 
 // This is for when user selects a different color using custom color picker.
  const customBtn = document.querySelector(".custom");
-  customBtn.addEventListener("input", () => hex = customBtn.value);
+ const blackBtn = document.querySelector(".black");
+ const eraserBtn = document.querySelector(".eraser");
+ const rainbowBtn = document.querySelector(".rainbow");
 
-// Generating rainbow colors
-const  rainbowBtn = document.querySelector(".rainbow");
+//  CUSTOM BUTTON
+  customBtn.addEventListener("input", () => {
+    hex = customBtn.value;
+  });
+
+
+
+
+  //RAINBOW BUTTON
 rainbowBtn.addEventListener("click", () => rainbowGen());
 
+// This function triggers when you click rainbow function and makes pen draw rainbow colors on the canvas
 function rainbowGen() {
 let red = "#FF0000";
 let orange = "#FFA500";
@@ -98,24 +110,44 @@ let green = "#008000";
 let blue = "#0000FF";
 let indigo = "#4B0082";
 let violet = "#EE82EE";
-
 const rainbowArr = [red, orange, yellow, green, blue, indigo, violet];
-let i = 0;
-document.addEventListener("mouseover", e => {
+
+//Checking if user is clicking and dragging on the canvas. If so we add rainbow color from rainbowArr to the background of the clicked div.
+isRainbowOn = true;
+ canvas.addEventListener("mouseover", rainbowColors);
+
+ let i = 0;
+  function rainbowColors(e){
+
   if(e.target.matches(".empty-div")){
     if (mouseDown == true){
-      e.target.style.backgroundColor = rainbowArr[i];
+      // To add new rainbow color each time we use variable i.
+      hex = rainbowArr[i];
+      e.target.style.backgroundColor = hex;
       i++;
-    }
+    } 
     if(i == 7){ // Resetting rainbow colors when we reach violet
       i = 0;
     }
   }
-} )
+  
+}
+blackBtn.addEventListener("click", () => canvas.removeEventListener("mouseover", rainbowColors));
+eraserBtn.addEventListener("click", () => canvas.removeEventListener("mouseover", rainbowColors));
+customBtn.addEventListener("click", () => canvas.removeEventListener("mouseover", rainbowColors));
 }
 
 
 
+// BLACK BUTTON
+blackBtn.addEventListener("click", () =>{
+  hex = blackBtn.value;
+})
+ 
+// ERASER BUTTON
+eraserBtn.addEventListener("click()", () => {
+  hex = eraserBtn.value;
+})
 
 
 
@@ -123,4 +155,6 @@ document.addEventListener("mouseover", e => {
 
 
 
-canvasColorInput();
+
+
+
